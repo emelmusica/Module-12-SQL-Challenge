085464e411d77316f60c860f2039b2e8859828dd
Em3L <martink_lopez@yahoo.com>
2023-08-13 16:25:41 +1000

    created role table

--- schema.sql
+++ schema.sql
@@ -5,3 +5,10 @@ CREATE TABLE department (
 );
 
 -- Create the Role table
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titles VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id iNT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
