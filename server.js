84d3aab50f5138ab42d23b64d2c00cca182b1939
Em3L <martink_lopez@yahoo.com>
2023-08-13 18:52:24 +1000

    deleted empty space

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const inquirer = require('inquirer');
 const mysql = require('mysql');
 const connection = require('./db/connection');
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
