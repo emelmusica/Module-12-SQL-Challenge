3d1fe68fc43590f871c072d8d0321cef79869a10
Em3L <martink_lopez@yahoo.com>
2023-08-13 14:28:52 +1000

    function to start the application to display a list of choices to the user

--- app.js
+++ app.js
@@ -3,3 +3,23 @@
 const inquirer = require('inquirer');
 const db =require('./db/queries');
 
+// function to start the application to display a list of choices to the user
+function startApp() {
+    inquirer.createPromptModule([
+        {
+            type: 'list',
+            name: 'action',
+            message: '', // don't forget to add message
+            choices: [
+                'View all departments',
+                'view all roles',
+                'View all employees', 
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an empoyee role',
+                'Exit'
+            ]
+        }
+    ])
+}
