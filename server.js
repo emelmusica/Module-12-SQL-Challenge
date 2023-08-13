0a34073ca501b365c057af64f7814121e631cff1
Em3L <martink_lopez@yahoo.com>
2023-08-13 18:28:53 +1000

    added functions

--- server.js
+++ server.js
@@ -8,46 +8,68 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
-// Define API 
-
-//Get Departments
-app.get('/api/departments', (req,res) => {
-// Fetch and respond query functions with retrieved data
+// Get Departments
+app.get('/api/departments', (req, res) => {
+  connection.query('SELECT * FROM department', (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
 });
 
-// Get roles
-app.get('/api/roles', (req, res) =>{
-
+// Get Roles
+app.get('/api/roles', (req, res) => {
+  connection.query('SELECT * FROM role', (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
 });
 
-// Get employees
-app.get('/api/employees', (req, res) =>{
-
+// Get Employees
+app.get('/api/employees', (req, res) => {
+  connection.query('SELECT * FROM employee', (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
 });
 
-// Add department
-app.post('/api/departments', (req, res) =>{
-const { name } = req.body;
+// Add Department
+app.post('/api/departments', (req, res) => {
+  const { name } = req.body;
+  connection.query('INSERT INTO department (name) VALUES (?)', [name], (err, results) => {
+    if (err) throw err;
+    res.json({ message: 'Department added successfully' });
+  });
 });
 
 // Add Role
 app.post('/api/roles', (req, res) => {
-    const { tittle, salary, department_id } = req.body;
+  const { title, salary, department_id } = req.body;
+  connection.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [title, salary, department_id], (err, results) => {
+    if (err) throw err;
+    res.json({ message: 'Role added successfully' });
+  });
 });
 
-// Add an employee
+// Add Employee
 app.post('/api/employees', (req, res) => {
-    const employeeId = req.params.id;
-    const { role_id } = req.body;
+  const { first_name, last_name, role_id, manager_id } = req.body;
+  connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [first_name, last_name, role_id, manager_id], (err, results) => {
+    if (err) throw err;
+    res.json({ message: 'Employee added successfully' });
+  });
 });
 
-// Update employee role
+// Update Employee Role
 app.put('/api/employees/:id', (req, res) => {
-    const employeeID = req.params.id;
-    const { role_id } = req.body;
+  const employeeID = req.params.id;
+  const { role_id } = req.body;
+  connection.query('UPDATE employee SET role_id = ? WHERE id = ?', [role_id, employeeID], (err, results) => {
+    if (err) throw err;
+    res.json({ message: 'Employee role updated successfully' });
+  });
 });
 
 // Start Server
 app.listen(PORT, () => {
-    console.log('Server is running on port ${PORT}');
+  console.log(`Server is running on port ${PORT}`);
 });
