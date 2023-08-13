f24010b73c54b1faf7cb0f4c4c8cf0d3e75e60f6
Em3L <martink_lopez@yahoo.com>
2023-08-13 15:40:31 +1000

    added const connection code

--- db/connection.js
+++ db/connection.js
@@ -1,2 +1,11 @@
 const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: '', // mysql username
+    password: '', // mysql password
+    database: '', // database name
+});
+
+module.exports = connection;
