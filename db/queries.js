073072c372f129d49ee87c435ab7d81ecac91fae
Em3L <martink_lopez@yahoo.com>
2023-08-13 15:18:44 +1000

    Created queries.js file import connect from connections and define DB

---
+++ db/queries.js
@@ -0,0 +1,10 @@
+// Import the 'connection' from 'connection.js'
+const connection = require('./connection');
+
+// Define a class "DB"
+class DB {
+    constructor(connection) {
+        this.connection = connection;
+    }
+}
+
