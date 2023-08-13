6b71e5aed22de5d14700a34e36d5319e814f8ea2
Em3L <martink_lopez@yahoo.com>
2023-08-13 16:47:24 +1000

    corrected: user: process.env.DB_USER, password: process.env.DB_PASSWORD, database: process.env.DB_DATABASE,

--- db/connection.js
+++ db/connection.js
@@ -3,9 +3,9 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: '', // mysql username
-    password: '', // mysql password
-    database: '', // database name
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
 
 module.exports = connection;
