8b6988b638b2d28cb74246f68ab202c64b617ac1
Em3L <martink_lopez@yahoo.com>
2023-08-14 19:34:32 +1000

    idk

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO viewDepartment (name)
+INSERT INTO department (name)
 VALUES ("Security Department"), ("Energy Department"), ("Research & Development"), ("Hospitality Team"), ("Management");
 
-INSERT INTO viewRoles (title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES ("Security Manager", 75000.00, 1), ("Senior Research Scientist", 120000.00, 3), ("Lead Engineer", 110000.00, 4), ("Guest Relations Specialist", 50000.00, 4), ("Chief Executive Officer", 250000.00, 5);
 
-INSERT INTO View employees (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("James", "Smith", 1, 3), ("Elena", "Johnson", 2, 1), ("Michael", "Williams", 3, 2), ("Emily", "Brown", 5, 2), ("David", "Jones", 5, 2);
